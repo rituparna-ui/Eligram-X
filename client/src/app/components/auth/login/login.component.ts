5255fa120e2d89d896c1d6346ffa5d7f398dfc0e
Rituparna W <rituparnaw16@gmail.com>
2023-01-07 17:35:12 +0530

    FRONTEND: Complete login page

--- client/src/app/components/auth/login/login.component.ts
+++ client/src/app/components/auth/login/login.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css', './../shared/styles.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  showPassword: boolean = false;
+
+  loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
+}
