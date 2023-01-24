febed783cd8476572005a5ae37b67267157798d8
Rituparna Warwatkar <rituparnaw16@gmail.com>
2023-01-24 13:10:42 +0530

    Create ChangePassword component

--- client/src/app/components/user/settings/change-password/change-password.component.ts
+++ client/src/app/components/user/settings/change-password/change-password.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.css']
+  styleUrls: ['./change-password.component.css'],
 })
-export class ChangePasswordComponent {
+export class ChangePasswordComponent implements OnInit {
+  changePasswordForm: FormGroup = new FormGroup({});
+  showPassword = false;
+  showConfirmPassword = false;
 
+  ngOnInit(): void {
+    this.changePasswordForm = new FormGroup({
+      oldPassword: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      confirmPassword: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+        this.confirmPasswordValidator(),
+      ]),
+    });
+  }
+  onSubmit() {
+    if (this.changePasswordForm.invalid) {
+      return;
+    }
+    const form: {
+      oldPassword: string;
+      password: string;
+      confirmPassword: string;
+    } = this.changePasswordForm.value;
+    // this.authService.signUp(form);
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  confirmPasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (this.changePasswordForm.value.password != control.value) {
+        return { passwordMatch: true };
+      } else {
+        return null;
+      }
+    };
+  }
 }
